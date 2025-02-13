44536d928f77d3047edfbe56388243a07eed45a0
Pedro Tavares <pedro@ptavares.com>
2025-02-13 17:20:17 +0000

    Update manage.py

--- examples/django_reversion_example/manage.py
+++ examples/django_reversion_example/manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_reversion_example.settings')
